2259c089c6b3f3ce225add86b536dc0cadca8eb6
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2023-08-25 06:30:30 +0000

    conversion objects

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+// let score = "21";
+// let score = "21a";
+// let score = null;
+let score = undefined
+
+console.log(typeof(score));
+
+let valInNum = Number(score)
+console.log(valInNum)
+console.log(typeof valInNum);
+
+//String to number
+//"33" - 33
+//"33a", null, undefined - NaN - type is number, number, number
+//true - 1, false - 0
+
+
+//values to boolean - Boolean(score)
+// 0 - false else true
+// "" - false else true
+
+//String(score) - string
